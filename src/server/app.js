90e1562d9778db7645215114bb97d359bcf02de4
William Idakwo <30729966+vixen24@users.noreply.github.com>
2024-08-11 15:06:19 +0100

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const STATIC_ASSETS_PATH = path.resolve(`${__dirname}/../../static`);
+
+const app = express();
 
 // Serve front end assets which have been built by webpack
 app.use("/static", express.static(STATIC_ASSETS_PATH));
